22e50f03aa96c44891ca43d37ccdb8ead5241386
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-04-16 17:32:16 +0100

    Update help.js

--- others/help.js
+++ others/help.js
@@ -14,8 +14,8 @@ module.exports = {
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
     .setTitle("__Help__")
-  .setImage( "https://cdn.discordapp.com/attachments/740344442979876977/812508833880604753/931c44efb4f87684-gif-music-song-beats-animation-family-headphones-audio-att.gif")
-  .setDescription(`**[  SUPPORT  ](https://discord.gg/t8ZwbHpJFP)**   - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)
+  .setImage( "https://cdn.discordapp.com/attachments/740344442979876977/812508833880604753/931c44efb4f87684-gif-music-song-beats-animation-family-headphones-audio-att.gif") 
+  .setDescription(`**[  SUPPORT  ](https://discord.gg/t8ZwbHpJFP)**   - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)** - [VOTE] (https://top.gg/bot/769330211157835784/vote)
 
 prefix=\`${prefix}\`
 

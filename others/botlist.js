485ef98b6e70b31138516edaf7e2812d7204d986
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-04-24 15:00:08 +0300

    Update botlist.js

--- others/botlist.js
+++ others/botlist.js
@@ -18,7 +18,7 @@ module.exports = {
     message.reply(new MessageEmbed().setColor("#c219d8")
     .setTitle("Here is Bot-Lists this Bot is on!")
     .addField("add bot","**https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot**")
-    .addField("**top.gg", "https://top.gg/bot/769330211157835784/vote**")
+    .addField("top.gg", "**https://top.gg/bot/769330211157835784/vote**")
     .addField("discordbotlist","**https://discordbotlist.com/bots/music-bot-1793**")
     );
 

636035b160852b3337e2d529277d4b41c9f0bfcf
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-06-02 00:12:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('NzY5MzMwMjExMTU3ODM1Nzg0.X5Nclw.AXG-thwTfbYH00FAwRKwW9deBH4');
+client.login('NzY5MzMwMjExMTU3ODM1Nzg0.X5Nclw.5IcE4-lmbC-7_o3mIZqvd1TbrX0');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;

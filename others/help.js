fd0623d2412adda5afa95950790650e8e491a733
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-06-22 21:05:34 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,7 +15,7 @@ module.exports = {
     .setColor('RANDOM')
     .setTitle("__Help__")
   .setImage("https://cdn.discordapp.com/attachments/808760849867079721/838228786742820904/standard_1.gif") 
-  .setDescription(`**[  SUPPORT  ](https://discord.gg/t8ZwbHpJFP)**    **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot) ** [ VOTE ] **(https://top.gg/bot/769330211157835784/vote)**   
+  .setDescription(`**[  SUPPORT  ](https://discord.gg/t8ZwbHpJFP)**    **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot) ** [  VOTE  ] (https://top.gg/bot/769330211157835784/vote)**   
 
  <a:emoji_5:793390710233497600>| prefix=\`${prefix}\`
 

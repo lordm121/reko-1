0cee1b0a2a463b97864596cce46073589964d49e
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-06-22 21:15:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -17,16 +17,16 @@ module.exports = {
   .setImage("https://cdn.discordapp.com/attachments/808760849867079721/838228786742820904/standard_1.gif") 
   .setDescription(`**[  SUPPORT  ](https://discord.gg/t8ZwbHpJFP)**    **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)**   **[  VOTE  ](https://top.gg/bot/769330211157835784/vote)**
 
- <a:emoji_5:793390710233497600>| prefix=\`${prefix}\`
+ <a:emoji_5:793390710233497600> | prefix=\`${prefix}\`
 
 
-<a:emoji_1:849776457122185277>| Other
+<a:emoji_1:849776457122185277> | Other
 
 \`help\`      \`ping\` 
 \`prefix\`    \`uptime\`
 \`botlist\`
 
-<a:emoji_1:849776441126158337>|Music 
+<a:emoji_1:849776441126158337> | Music 
 
 \`filter\` - \`loop \` - \`lyrics\` - \`np\`
 \`pause\` - \`play\` - \`queue\` - \`radio\` - \`remove\`

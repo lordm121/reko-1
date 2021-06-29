adff9ca7153641e2c5622b81b7c807de639e5a55
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-06-29 04:32:56 +0300

    Update play.js

--- Music/play.js
+++ Music/play.js
@@ -5,8 +5,8 @@ const { play } = require("../include/play");
 const { Client, Collection, MessageEmbed } = require("discord.js");
 const { attentionembed } = require("../util/attentionembed");
 const { PREFIX } = require(`../config.json`);
-//const ytsr = require("youtube-sr")
-const yts = require("yt-search");
+const ytsr = require("ytsr")
+
 
 ////////////////////////////
 //////COMMAND BEGIN/////////
